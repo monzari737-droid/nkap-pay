eb2d39404d7dafd4eb5b6461846431f8fa61e513
monzari737-droid <monzari737@gmail.com>
2026-04-10 13:38:06 +0100

    names.js

--- names.js
+++ names.js
@@ -1,24 +1,13 @@
-const CASINO_NAMES = {
-    afrique: [
-        "Moussa", "Fatoumata", "Tidiane", "Aminata", "Bakary", "Oumou", "Cheick", "Mariam",
-        "Dieudonné", "Inès", "Junior", "Merveille", "Blaise", "Tatiana", "Guy", "Ornella",
-        "Kofi", "Ama", "Tendai", "Zanele", "Jomo", "Nala", "Thabo", "Kwamé"
-    ],
-    europe: [
-        "Lucas", "Emma", "Gabriel", "Chloé", "Hugo", "Léa", "Thomas", "Manon",
-        "Mateo", "Sofia", "Alessandro", "Giulia", "Diego", "Lucia", "Marco", "Elena",
-        "Lukas", "Hanna", "Erik", "Astrid", "Klaus", "Ingrid", "Stefan", "Helga"
-    ],
-    usa: [
-        "Jackson", "Olivia", "Liam", "Ava", "Noah", "Isabella", "Mason", "Sophia",
-        "Tyler", "Madison", "Hunter", "Brooklyn", "Jayden", "Harper", "Logan", "Riley"
-    ]
-};
+window.CASINO_NAMES = [
+    "Moussa", "Fatou", "Kamga", "Simo", "Ondoa", "Zambo", "Lucas", "Emma", 
+    "Thomas", "Mateo", "Sofia", "Lukas", "Hanna", "Jackson", "Olivia", "Liam"
+];
 
-// Fonction pour récupérer un nom aléatoire flouté
-function getRandomBlurredName() {
-    const categories = Object.keys(CASINO_NAMES);
-    const category = categories[Math.floor(Math.random() * categories.length)];
-    const name = CASINO_NAMES[category][Math.floor(Math.random() * CASINO_NAMES[category].length)];
-    return name.substring(0, 2) + "****";
-}
+window.getRandomUser = function() {
+    const name = window.CASINO_NAMES[Math.floor(Math.random() * window.CASINO_NAMES.length)];
+    return {
+        name: name.substring(0, 2) + "****",
+        amount: Math.floor(Math.random() * 500) + 1,
+        num: Math.floor(Math.random() * 6)
+    };
+};
